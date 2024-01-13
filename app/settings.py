2fd60e653b889e38e0c41a00300ff0df607748d4
Intense Sunlight <intensesunlight@gmail.com>
2024-01-13 23:09:04 +0100

    added app.settings module and pydantic requirement

---
+++ app/settings.py
@@ -0,0 +1,123 @@
+import os
+import json
+from dotenv import load_dotenv
+from pydantic import BaseModel, field_serializer
+from typing import Optional, List, Dict, Union, TextIO
+
+__all__ = ["Settings"]
+
+
+# Default settings
+class ServerModel(BaseModel):
+    host: Optional[str] = "127.0.0.1"
+    port: Optional[int] = 8188
+    sd_api_name: Optional[str] = "comfyUI"
+    discord_bot_key: Optional[str] = "fake"  # must be supplied in .env file
+
+    @field_serializer("discord_bot_key", when_used="json")
+    def _hide_discord_bot_key(cls, v: str) -> str:
+        return "*" * 8
+
+
+# Default command settings
+class CommandsModel(BaseModel):
+    generate_random_command: str = "generate_random"
+    generate_txt2img: str = "generate"
+    generate_txt2vid: str = "generate_txt2video"
+    generate_img2vid: str = "generate_img2video"
+
+
+# Default image file settings
+class FilesModel(BaseModel):
+    image_folder: Union[str, os.PathLike] = "./GeneratedImages"
+    workflows_folder: Union[str, os.PathLike] = "./app/sd_apis/comfyUI_workflows"
+    image_types: List[str] = ["jpg", "png", "jpeg"]
+    default_image_type: str = "png"
+    video_types: List[str] = ["mp4", "gif"]
+    default_video_type: str = "gif"
+
+    # Default txt2img settings
+
+
+class Txt2ImgSingleModel(BaseModel):
+    display_name: str = "default_v1.5"
+    sd_model: str = "v1-5-pruned-emaonly.ckpt"
+    width: Optional[int] = 512
+    height: Optional[int] = 512
+    workflow_api: Optional[str] = "default_api.json"
+    workflow_api_map: Optional[str] = "default_api_map.json"
+
+
+class Txt2ImgContainerModel(BaseModel):
+    variation_strength: float = 0.0
+    upscaler_model: str = "4x_NMKD-Siax_200k"
+    n_images: int = 2  # number of images to generate per request
+    models: Dict[str, Txt2ImgSingleModel] = {
+        Txt2ImgSingleModel().display_name: Txt2ImgSingleModel()
+    }
+
+    def add_model(self, model_dict: Dict):
+        model = Txt2ImgSingleModel(**model_dict)
+        self.models.update({model.display_name: model})
+
+
+class _Settings(BaseModel):
+    server: ServerModel = ServerModel()
+    commands: CommandsModel = CommandsModel()
+    files: FilesModel = FilesModel()
+    txt2img: Txt2ImgContainerModel = Txt2ImgContainerModel()
+
+    def __init__(
+        self,
+        *args,
+        dot_env: Union[str, os.PathLike, TextIO] = None,
+        json_file: Union[str, os.PathLike, TextIO] = None,
+        **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        if json_file is not None:
+            self.load_json(json_file)
+
+        if dot_env is not None:
+            self.load_dotenv(dot_env)
+
+    def reset(self):
+        self.__dict__.update(self.__class__().__dict__)
+
+    def load_json(self, json_file: Union[str, os.PathLike, TextIO]):
+        if isinstance(json_file, str):
+            with open(json_file, "r") as f:
+                new_self = self.__class__(**json.load(f))
+                self.__dict__.update(new_self.__dict__)
+        else:
+            new_self = self.__class__(**json.load(json_file))
+            self.__dict__.update(new_self.__dict__)
+
+    def load_dotenv(self, dotenv_path: Union[str, os.PathLike, TextIO]):
+        load_dotenv(dotenv_path=dotenv_path, override=True)
+
+        # legacy support for .env file
+        self.server.discord_bot_key = os.getenv("BOT_KEY", self.server.discord_bot_key)
+        self.server.host = os.getenv("SD_HOST", self.server.host)
+        self.server.port = os.getenv("SD_PORT", self.server.port)
+        self.server.sd_api_name = os.getenv("SD_API", self.server.sd_api_name)
+        self.commands.generate_random_command = os.getenv(
+            "BOT_GENERATE_RANDOM_COMMAND",
+            self.commands.generate_random_command,
+        )
+        self.commands.generate_txt2img = os.getenv(
+            "BOT_GENERATE_COMMAND", self.commands.generate_txt2img
+        )
+        self.txt2img.variation_strength = os.getenv(
+            "SD_VARIATION_STRENGTH", self.txt2img.variation_strength
+        )
+
+    def save_json(self, json_file: Union[str, os.PathLike, TextIO]):
+        if isinstance(json_file, (str, os.PathLike)):
+            with open(json_file, "w") as f:
+                f.write(self.model_dump_json(indent=4))
+        else:
+            json_file.write(self.model_dump_json(indent=4))
+
+
+Settings = _Settings()  # create singleton instance
---
+++ app/tests/test_settings.py
@@ -0,0 +1,86 @@
+import os
+import re
+import unittest
+from app.settings import Settings
+
+
+class TestSettings(unittest.TestCase):
+    def test_default_settings(self):
+        server = Settings.server
+        host = Settings.server.host
+        bot_key = Settings.server.discord_bot_key
+        commands = Settings.commands
+        files = Settings.files
+        txt2img = Settings.txt2img
+        models = Settings.txt2img.models
+
+        self.assertIsNotNone(server)
+        self.assertIsNotNone(host)
+        self.assertIsNotNone(bot_key)
+        self.assertIsNotNone(commands)
+        self.assertIsNotNone(files)
+        self.assertIsNotNone(txt2img)
+        self.assertIsNotNone(models.get("default_v1.5"))
+
+        default_model = models.get("default_v1.5")
+        self.assertEqual(default_model.width, 512)
+        self.assertEqual(default_model.height, 512)
+        self.assertEqual(default_model.sd_model, "v1-5-pruned-emaonly.ckpt")
+        self.assertEqual(default_model.workflow_api, "default_api.json")
+        self.assertEqual(default_model.workflow_api_map, "default_api_map.json")
+
+    def test_reset(self):
+        Settings.server.port = 8189
+        Settings.reset()
+        self.assertEqual(Settings.server.port, 8188)
+
+    def test_add_image_model(self):
+        Settings.txt2img.add_model(
+            {
+                "display_name": "test_model",
+                "sd_model": "test_model.ckpt",
+                "width": 256,
+                "height": 256,
+                "workflow_api": "test_api.json",
+                "workflow_api_map": "test_api_map.json",
+            }
+        )
+        self.assertIsNotNone(Settings.txt2img.models.get("test_model"))
+        self.assertEqual(len(Settings.txt2img.models), 2)
+
+    def test_settings_model(self):
+        # test change of value
+        orig_json_schema = Settings.model_dump_json(indent=4)
+        orig_json_schema = re.sub(r'"port": 8188', r'"port": 8189', orig_json_schema)
+        with open("test_settings_model.json", "w") as f:
+            f.write(orig_json_schema)
+
+        with open("test_settings_model.json", "r") as f:
+            Settings.load_json(f)
+        json_schema = Settings.model_dump_json(indent=4)
+        self.assertEqual(Settings.server.port, 8189)
+
+        # test save methods
+        Settings.load_json("test_settings_model.json")
+        json_schema = Settings.model_dump_json(indent=4)
+        self.assertEqual(orig_json_schema, json_schema)
+
+        with open("test_settings2_model.json", "w") as f:
+            Settings.save_json(f)
+
+        with open("test_settings2_model.json", "r") as f:
+            Settings.load_json(f)
+
+        self.assertEqual(orig_json_schema, Settings.model_dump_json(indent=4))
+
+        Settings.server.port = 8190
+        Settings.save_json("test_settings2_model.json")
+        Settings.load_json("test_settings2_model.json")
+        self.assertEqual(Settings.server.port, 8190)
+
+    def tearDown(self):
+        if os.path.exists("test_settings_model.json"):
+            os.remove("test_settings_model.json")
+
+        if os.path.exists("test_settings2_model.json"):
+            os.remove("test_settings2_model.json")
