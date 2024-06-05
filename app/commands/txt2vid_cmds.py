4afaab688cfc149ec5f0384f3ce34904d86d0465
Intense Sunlight <intensesunlight@gmail.com>
2024-06-05 23:27:14 +0200

    separate 1step and 2step animation type commands

--- app/commands/txt2vid_cmds.py
+++ app/commands/txt2vid_cmds.py
@@ -1,75 +1,57 @@
-import os
 import discord
-import asyncio
-from typing import List, cast
+from typing import List
 from app.sd_apis.api_handler import Sd
-from app.utils.async_task_queue import AsyncTaskQueue, Task
-from app.utils.helpers import random_seed, CARDINALS
-from app.utils import GeneratePrompt, Orientation, ImageCount, PromptConstants
-from app.settings import Settings, GroupCommands, Txt2ImgSingleModel
-from app.utils.image_file import ImageFile, ImageContainer
-from app.commands.txt2img_cmds import process_image, Txt2ImageCommands
-from app.views.generate_animation import GenerateAnimationPreviewView
-from app.views.view_helpers import create_animation
-
-
-# -------------------------------
-# Helper functions
-# -------------------------------
-# image processing function
-async def process_animation(
-    i: int,
-    image: ImageContainer,
-    n_images: int,
-    response: discord.ApplicationContext,
-) -> ImageContainer:
-    image.image: ImageFile = await asyncio.to_thread(create_animation, image, Sd.api)
-
-    cardinal = CARDINALS[min(i, len(CARDINALS) - 1)]
-    percent = int((i + 1) / n_images * 100)
-    try:
-        await response.edit_original_response(
-            content=f"Generated the {cardinal} image...({percent}%)"
-        )
-    except discord.errors.NotFound:
-        pass
-
-    return image
+from app.utils.helpers import CARDINALS
+from app.utils import Orientation, ImageCount, PromptConstants
+from app.settings import Settings, GroupCommands
+from app.views.generate_animation_2step import (
+    GenerateAnimationView,
+    GenerateAnimationPreviewView,
+)
+from .txt_base_cmds import TxtCommandsMixin
 
 
 # -------------------------------
 # Txt2ImageCommands definition
 # -------------------------------
-class Txt2VideoCommands(Txt2ImageCommands):
+class Txt2Video1StepCommands(TxtCommandsMixin):
 
     def __init__(
         self,
         sub_group: discord.SlashCommandGroup,
-        commands: List[str] = ["random_animation", "animation"],
+        commands: List[str] = ["random_animation_quick", "animation_quick"],
     ):
-        super().__init__(sub_group, commands=[])
+        super().__init__(sub_group=sub_group)
 
         # subcommands must be bound in the constructor
         # all subcommands must be ascync functions
         if not commands:
             raise ValueError("No commands specified for Txt2VideoCommands")
-        if "random_animation" in commands:
+
+        if "random_animation_quick" in commands:
             self.bind(
-                self.random_animation, "random", "Generate animation from random prompt"
+                self.random_animation_1step,
+                "quick_random",
+                "Generate quick animation from random prompt",
             )
-        if "animation" in commands:
-            self.bind(self.generate_animation, "image", "Generate animation from text")
+
+        if "animation_quick" in commands:
+            self.bind(
+                self.generate_animation_1step,
+                "quick_animate",
+                "Generate quick animation from text",
+            )
 
     # -------------------------------
-    # Random Image
+    # Random Image 1 step
     # -------------------------------
-    async def random_animation(
+    async def random_animation_1step(
         self,
         ctx: discord.ApplicationContext,
         model: discord.Option(
             str,
-            choices=list(Settings.txt2vid.models.keys()),
-            default=list(Settings.txt2vid.models.keys())[0],
+            choices=list(Settings.txt2vid1step.models.keys()),
+            default=list(Settings.txt2vid1step.models.keys())[0],
             description="Which model should be used?",
         ),
         orientation: discord.Option(
@@ -83,7 +65,166 @@ class Txt2VideoCommands(Txt2ImageCommands):
             await ctx.respond("This command cannot be used in direct messages.")
             return
 
-        model_def = Settings.txt2vid.models[model]
+        model_def = Settings.txt2vid1step.models[model]
+        images, title_prompts, response = await self._random_animation(
+            ctx=ctx,
+            model_def=model_def,
+            workflow_api_file=model_def.workflow_api,
+            workflow_api_map_file=model_def.workflow_api_map,
+            orientation=orientation,
+        )
+        if images is None:
+            return
+
+        command_name = f"{Settings.server.bot_command}.{GroupCommands.txt2vid1step.name}.{ctx.command.name}"
+        prompt_description = "\n".join(
+            f"Prompt ({CARDINALS[min(i, len(CARDINALS)-1)]}): `{title_prompts[i]}`"
+            for i in range(len(title_prompts))
+        )
+        embed = discord.Embed(
+            title=f"Generated {len(images)} random images using these settings:",
+            description=(
+                f"{prompt_description}\n"
+                f"Orientation: `{orientation}`\n"
+                f"Negative Prompt: `{images[0].negative_prompt}`\n"
+                f"Model: `{model}`\n"
+                f"Total generated images: `{ImageCount.get_count()}`\n\n"
+                f"Want to generate your own image? Type your prompt and style after `/{command_name}`"
+            ),
+            color=discord.Colour.blurple(),
+        )
+
+        message = await ctx.respond(
+            f"<@{ctx.author.id}>'s Random Generations:",
+            files=[discord.File(img.image.image_filename) for img in images],
+            view=GenerateAnimationView(
+                images=images,
+                sd_api=Sd.api,
+            ),
+            embed=embed,
+        )
+        await message.add_reaction("👍")
+        await message.add_reaction("👎")
+        await response.delete_original_response()
+
+    # -------------------------------
+    # Generate Image
+    # -------------------------------
+    async def generate_animation_1step(
+        self,
+        ctx: discord.ApplicationContext,
+        prompt: discord.Option(str, description="What do you want to generate?"),
+        style: discord.Option(
+            str,
+            choices=PromptConstants.get_style_presets(),
+            description="In which style should the image be?",
+        ),
+        model: discord.Option(
+            str,
+            choices=list(Settings.txt2vid1step.models.keys()),
+            default=list(Settings.txt2vid1step.models.keys())[0],
+            description="Which model should be used?",
+        ),
+        orientation: discord.Option(
+            str,
+            choices=Orientation.get_orientation_presets(),
+            default=Orientation.SQUARE,
+            description="In which orientation should the image be?",
+        ),
+        negative_prompt: discord.Option(
+            str, description="What do you want to avoid?", default=""
+        ),
+    ):
+        if ctx.guild is None and not Settings.server.allow_dm:
+            await ctx.respond("This command cannot be used in direct messages.")
+            return
+
+        model_def = Settings.txt2img.models[model]
+        images, response = await self._generate_animation(
+            ctx=ctx,
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            style=style,
+            model_def=model_def,
+            workflow_api_file=model_def.workflow_api,
+            workflow_api_map_file=model_def.workflow_api_map,
+            orientation=orientation,
+        )
+
+        command_name = f"{Settings.server.bot_command}.{GroupCommands.txt2vid1step.name}.{ctx.command.name}"
+
+        embed = discord.Embed(
+            title="Prompt: " + prompt,
+            description=(
+                f"Style: `{style}`\n"
+                f"Orientation: `{orientation}`\n"
+                f"Negative Prompt: `{negative_prompt}`\n"
+                f"Total generated images: `{ImageCount.get_count()}`\n\n"
+                f"Want to generate your own image? Type your prompt and style after `/{command_name}`"
+            ),
+            color=discord.Colour.blurple(),
+        )
+
+        message = await ctx.respond(
+            f"<@{ctx.author.id}>'s Generations:",
+            files=[discord.File(img.image.image_filename) for img in images],
+            view=GenerateAnimationView(
+                images=images,
+                sd_api=Sd.api,
+            ),
+            embed=embed,
+        )
+
+        await message.add_reaction("👍")
+        await message.add_reaction("👎")
+        await response.delete_original_response()
+
+
+class Txt2Video2StepCommands(TxtCommandsMixin):
+    def __init__(
+        self,
+        sub_group: discord.SlashCommandGroup,
+        commands: List[str] = ["random_animation", "animation"],
+    ):
+        super().__init__(sub_group=sub_group)
+
+        # subcommands must be bound in the constructor
+        # all subcommands must be ascync functions
+        if not commands:
+            raise ValueError("No commands specified for Txt2VideoCommands")
+        if "random_animation" in commands:
+            self.bind(
+                self.random_animation, "random", "Generate animation from random prompt"
+            )
+        if "animation" in commands:
+            self.bind(
+                self.generate_animation, "animate", "Generate animation from text"
+            )
+
+    # -------------------------------
+    # Random Image 2 step
+    # -------------------------------
+    async def random_animation(
+        self,
+        ctx: discord.ApplicationContext,
+        model: discord.Option(
+            str,
+            choices=list(Settings.txt2vid2step.models.keys()),
+            default=list(Settings.txt2vid2step.models.keys())[0],
+            description="Which model should be used?",
+        ),
+        orientation: discord.Option(
+            str,
+            choices=["Square", "Portrait", "Landscape"],
+            default="Square",
+            description="In which orientation should the image be?",
+        ),
+    ):
+        if ctx.guild is None and not Settings.server.allow_dm:
+            await ctx.respond("This command cannot be used in direct messages.")
+            return
+
+        model_def = Settings.txt2vid2step.models[model]
         images, title_prompts, response = await self._random_image(
             ctx=ctx,
             model_def=model_def,
@@ -94,9 +235,7 @@ class Txt2VideoCommands(Txt2ImageCommands):
         if images is None:
             return
 
-        command_name = (
-            f"{Settings.server.bot_command}.{GroupCommands.txt2vid.name}.random"
-        )
+        command_name = f"{Settings.server.bot_command}.{GroupCommands.txt2vid2step.name}.{ctx.command.name}"
         prompt_description = "\n".join(
             f"Prompt ({CARDINALS[min(i, len(CARDINALS)-1)]}): `{title_prompts[i]}`"
             for i in range(len(title_prompts))
@@ -128,7 +267,7 @@ class Txt2VideoCommands(Txt2ImageCommands):
         await response.delete_original_response()
 
     # -------------------------------
-    # Generate Image
+    # Generate Image 2 step
     # -------------------------------
     async def generate_animation(
         self,
@@ -141,8 +280,8 @@ class Txt2VideoCommands(Txt2ImageCommands):
         ),
         model: discord.Option(
             str,
-            choices=list(Settings.txt2vid.models.keys()),
-            default=list(Settings.txt2vid.models.keys())[0],
+            choices=list(Settings.txt2vid2step.models.keys()),
+            default=list(Settings.txt2vid2step.models.keys())[0],
             description="Which model should be used?",
         ),
         orientation: discord.Option(
@@ -159,7 +298,7 @@ class Txt2VideoCommands(Txt2ImageCommands):
             await ctx.respond("This command cannot be used in direct messages.")
             return
 
-        model_def = Settings.txt2vid.models[model]
+        model_def = Settings.txt2vid2step.models[model]
         images, response = await self._generate_image(
             ctx=ctx,
             prompt=prompt,
--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ from app.utils.async_task_queue import AsyncTaskQueue
 from app.commands.txt2img_cmds import Txt2ImageCommands
 from app.commands.img2img_cmds import Img2ImageCommands, UpscalerCommands
 from app.commands.img2vid_cmds import Img2VideoCommands
-from app.commands.txt2vid_cmds import Txt2VideoCommands
+from app.commands.txt2vid_cmds import Txt2Video1StepCommands, Txt2Video2StepCommands
 
 
 # upfront checks
@@ -107,12 +107,19 @@ if Settings.has_command(GroupCommands.img2vid):
     )
     Img2VideoCommands(img2vid_group)
 
-# txt2vid
-if Settings.has_command(GroupCommands.txt2vid):
-    txt2vid_group = Bot.create_subgroup(
-        GroupCommands.txt2vid.name, "Create video from text"
+# txt2vid1step
+if Settings.has_command(GroupCommands.txt2vid1step):
+    txt2vid1step_group = Bot.create_subgroup(
+        GroupCommands.txt2vid1step.name, "Create quick animation from text"
     )
-    Txt2VideoCommands(txt2vid_group)
+    Txt2Video1StepCommands(txt2vid1step_group)
+
+# txt2vid2step
+if Settings.has_command(GroupCommands.txt2vid2step):
+    txt2vid2step_group = Bot.create_subgroup(
+        GroupCommands.txt2vid2step.name, "Create animation from text"
+    )
+    Txt2Video2StepCommands(txt2vid2step_group)
 
 # upscaler
 if Settings.has_command(GroupCommands.upscaler):
